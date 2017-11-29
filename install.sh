8e0906da329dd1b786fe12e046f861a084a2dd0a
Luclu7 <luclu7@luclu7.fr>
2017-11-29 23:54:56 +0100

    beginning of installation script and licence

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "______          _    ___________ _   _ "
+echo "|  ___|        | |  /  __ \  _  \ \ | |"
+echo "| |_ _   _  ___| | _| /  \/ | | |  \| |"
+echo "|  _| | | |/ __| |/ / |   | | | | . ' |"
+echo "| | | |_| | (__|   <| \__/\ |/ /| |\  |"
+echo "\_|  \__,_|\___|_|\_\____/___/ \_| \_/"
+
+echo "This script will install a FuckCDN instance on /opt/fuckcdn."
