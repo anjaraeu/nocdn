2b7a82350a6cc33052d3e4d74b813781d893af62
Luclu7 <luclu7@luclu7.fr>
2017-12-10 23:48:09 +0100

    name migration

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+echo '  _   _        _____ _____  _   _ '
+echo ' | \ | |      / ____|  __ \| \ | |'
+echo ' |  \| | ___ | |    | |  | |  \| |'
+echo ' | . ` |/ _ \| |    | |  | | ` ` |'
+echo ' | |\  | (_) | |____| |__| | |\  |'
+echo ' |_| \_|\___/ \_____|_____/|_| \_|'
+                                  
+echo 'This script will install a NoCDN instance on /opt/nocdn.'
 
-echo "______          _    ___________ _   _ "
-echo "|  ___|        | |  /  __ \  _  \ \ | |"
-echo "| |_ _   _  ___| | _| /  \/ | | |  \| |"
-echo "|  _| | | |/ __| |/ / |   | | | | . ' |"
-echo "| | | |_| | (__|   <| \__/\ |/ /| |\  |"
-echo "\_|  \__,_|\___|_|\_\____/___/ \_| \_/"
 
-echo "This script will install a FuckCDN instance on /opt/fuckcdn."
