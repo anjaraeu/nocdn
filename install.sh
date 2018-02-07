5a7cab4f2a122423b3337b3dbd9abba61045394e
Skid <skid@tuto-craft.com>
2018-02-07 18:13:41 +0100

    Fix script

--- install.sh
+++ install.sh
@@ -36,27 +36,31 @@ function install_nginx_arch {
 }
 
 function install_config {
-echo "Installing NoCDN files ..."
-git clone https://github.com/nsaovh/nocdn /srv/nocdn
-echo "Installing nginx config ..."
-cp /srv/nocdn/conf/nocdn1.conf /etc/nginx/sites-enabled/nocdn1.conf
-cp /srv/nocdn/conf/nocdn2.conf /etc/nginx/sites-enabled/nocdn2.conf
-sed -i 's|domain.tld|$domain|' /etc/nginx/sites-enabled/nocdn1.conf
+	echo "Installing NoCDN files ..."
+	git clone https://github.com/nsaovh/nocdn /srv/nocdn
+	echo "Installing nginx config ..."
+	cp /srv/nocdn/conf/nocdn1.conf /etc/nginx/sites-enabled/nocdn1.conf
+	cp /srv/nocdn/conf/nocdn2.conf /etc/nginx/sites-enabled/nocdn2.conf
+	sed -i 's|domain.tld|$domain|' /etc/nginx/sites-enabled/nocdn1.conf
 
-read -r -p "Is acme.sh already installed in /root/.acme.sh ? [y/N] " response
-response=${response,,}    # tolower
-if [[ "$response" =~ ^(yes|y)$ ]] ; then
-fi
-if [[ "$response" =~ ^(no|n)$ ]] ; then
-	echo "Installing acme.sh ..."
-	git clone https://github.com/Neilpang/acme.sh /root/.acme.sh
-fi
-
-echo "Generating certificates..."
-/root/acme.sh/acme.sh --issue --webroot /srv/nocdn/public -k 4096 -d $domain
-openssl req -x509 -newkey rsa:4096 -sha256 -utf8 -days 3650 -nodes -config /srv/nocdn/conf/openssl.conf -keyout /srv/nocdn/certs/key.pem -out /srv/nocdn/certs/cert.pem
-echo "Restarting nginx ..."
-systemctl restart nginx
+	read -r -p "Is acme.sh already installed in /root/.acme.sh ? [y/N] " response
+	response=${response,,}    # tolower
+	if [[ "$response" =~ ^(yes|y)$ ]] ; then
+		echo "Generating certificates..."
+		/root/acme.sh/acme.sh --issue --webroot /srv/nocdn/public -k 4096 -d $domain
+		openssl req -x509 -newkey rsa:4096 -sha256 -utf8 -days 3650 -nodes -config /srv/nocdn/conf/openssl.conf -keyout /srv/nocdn/certs/key.pem -out /srv/nocdn/certs/cert.pem
+		echo "Restarting nginx ..."
+		systemctl restart nginx
+	fi
+	if [[ "$response" =~ ^(no|n)$ ]] ; then
+		echo "Installing acme.sh ..."
+		git clone https://github.com/Neilpang/acme.sh /root/.acme.sh
+		echo "Generating certificates..."
+		/root/acme.sh/acme.sh --issue --webroot /srv/nocdn/public -k 4096 -d $domain
+		openssl req -x509 -newkey rsa:4096 -sha256 -utf8 -days 3650 -nodes -config /srv/nocdn/conf/openssl.conf -keyout /srv/nocdn/certs/key.pem -out /srv/nocdn/certs/cert.pem
+		echo "Restarting nginx ..."
+		systemctl restart nginx
+	fi
 }
 
 function success {
